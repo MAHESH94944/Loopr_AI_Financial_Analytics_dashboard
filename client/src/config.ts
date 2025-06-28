3e07b66cf73db3b68d7a6eca137704fa59c177b8
Mahesh Jadhao <maheshjadhao172@gmail.com>
2025-06-28 18:52:08 +0530

    first commit

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   apiUrl:
     import.meta.env.VITE_API_URL ||
-    "https://loopr-ai-financial-analytics-dashboard-1.onrender.com",
+    "https://loopr-ai-financial-analytics-dashboard-1.onrender.com/api",
 };
 
 export default config;
